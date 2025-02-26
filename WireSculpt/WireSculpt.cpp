afe6725892128d7a52f59f66cb81132913dd60ea
Neha Thumu <71038542+thumun@users.noreply.github.com>
2025-02-26 13:32:52 -0500

    setup repo

---
+++ WireSculpt/WireSculpt.cpp
@@ -0,0 +1,84 @@
+#include "WireSculpt.h"
+#include <maya/MFnPlugin.h>
+
+// define EXPORT for exporting dll functions
+#define EXPORT _declspec(dllexport)
+
+const char* nameFlag = "-n";
+const char* nameLongFlag = "-name";
+const char* idFlag = "-id";
+const char* idLongFlag = "-idnumber";
+
+void* WireSculpt::creator()
+{
+	return new WireSculpt;
+}
+
+MSyntax WireSculpt::newSyntax()
+{
+	MSyntax syntax;
+	syntax.addFlag(nameFlag, nameLongFlag, MSyntax::kString);
+	syntax.addFlag(idFlag, idLongFlag, MSyntax::kUnsigned);
+	return syntax;
+}
+
+// Plugin doIt function
+MStatus WireSculpt::doIt(const MArgList& argList)
+{
+	MGlobal::displayInfo("about to add script");
+
+	MStatus status;
+
+	MString firstFlagArg;
+	unsigned int secondFlagArg{};
+
+	MArgParser argData(syntax(), argList, &status);
+
+	if (argData.isFlagSet(nameFlag)) {
+		argData.getFlagArgument(nameFlag, 0, firstFlagArg);
+	}
+	else {
+		MGlobal::displayInfo("Flag -name is NOT set.");
+	}
+
+	if (argData.isFlagSet(idFlag)) {
+		argData.getFlagArgument(idFlag, 0, secondFlagArg);
+	}
+	else {
+		MGlobal::displayInfo("Flag -id is NOT set.");
+	}
+
+	MGlobal::displayInfo("pop-up menu");
+
+	MString result;
+
+	MString dialog = "confirmDialog -title \"Hello Maya\" -message \"Name: ";
+	dialog += firstFlagArg;
+	dialog += "\\nID: ";
+	dialog += secondFlagArg;
+	dialog += "\" -button \"OK\" -defaultButton \"OK\" -dismissString \"Cancel\";";
+	MGlobal::executeCommand(dialog, result);
+
+	return MStatus::kSuccess;
+}
+
+// Initialize Maya Plugin upon loading
+EXPORT MStatus initializePlugin(MObject obj)
+{
+	MStatus status;
+	MFnPlugin plugin(obj, "CIS660", "1.0", "Any");
+	status = plugin.registerCommand("WireSculpt", WireSculpt::creator, WireSculpt::newSyntax);
+	if (!status)
+		status.perror("registerCommand failed");
+	return status;
+}
+// Cleanup Plugin upon unloading
+EXPORT MStatus uninitializePlugin(MObject obj)
+{
+	MStatus status;
+	MFnPlugin plugin(obj);
+	status = plugin.deregisterCommand("WireSculpt");
+	if (!status)
+		status.perror("deregisterCommand failed");
+	return status;
+}
---
+++ WireSculpt/WireSculpt.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <maya/MArgList.h>
+#include <maya/MObject.h>
+#include <maya/MGlobal.h>
+#include <maya/MPxCommand.h>
+#include <maya/MSyntax.h>
+#include <maya/MArgDatabase.h>
+#include <maya/MArgParser.h>
+
+
+class WireSculpt : public MPxCommand
+{
+public: 
+	WireSculpt() {};
+	virtual MStatus doIt(const MArgList& args);
+	static void* creator();
+	static MSyntax newSyntax();
+};
+
