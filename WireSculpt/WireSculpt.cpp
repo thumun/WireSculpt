6782c51c87b1b0fca7b44a6e242907d740351a77
Neha Thumu <71038542+thumun@users.noreply.github.com>
2025-03-03 17:06:20 -0500

    flags for UI added

--- WireSculpt/WireSculpt.cpp
+++ WireSculpt/WireSculpt.cpp
@@ -5,10 +5,32 @@
 // define EXPORT for exporting dll functions
 #define EXPORT _declspec(dllexport)
 
-const char* nameFlag = "-n";
-const char* nameLongFlag = "-name";
-const char* idFlag = "-id";
-const char* idLongFlag = "-idnumber";
+const char* fileFlag = "-fp";
+const char* fileLongFlag = "-filepath";
+
+const char* attractStrengthFlag = "-asr";
+const char* attractStrengthLongFlag = "-attractStrength";
+
+const char* attractSteepFlag = "-ase";
+const char* attractSteepLongFlag = "-attractSteep";
+
+const char* repulsionStrengthFlag = "-rsr";
+const char* repulsionStrengthLongFlag = "-repulsionStrength";
+
+const char* repulsionSteepFlag = "-rse";
+const char* repulsionSteepLongFlag = "-repulsionSteep";
+
+const char* expandFlag = "-e";
+const char* expandLongFlag = "-expand";
+
+const char* keepFlag = "-k";
+const char* keepLongFlag = "-keep";
+
+const char* lambdaFlag = "-l";
+const char* lambdaLongFlag = "-lambda";
+
+const char* thicknessFlag = "-t";
+const char* thicknessLongFlag = "-thickness";
 
 void* WireSculpt::creator()
 {
@@ -18,47 +40,121 @@ void* WireSculpt::creator()
 MSyntax WireSculpt::newSyntax()
 {
 	MSyntax syntax;
-	syntax.addFlag(nameFlag, nameLongFlag, MSyntax::kString);
-	syntax.addFlag(idFlag, idLongFlag, MSyntax::kUnsigned);
+
+	syntax.addFlag(fileFlag, fileLongFlag, MSyntax::kString);
+	syntax.addFlag(attractStrengthFlag, attractStrengthLongFlag, MSyntax::kDouble);
+	syntax.addFlag(attractSteepFlag, attractSteepLongFlag, MSyntax::kDouble);
+	syntax.addFlag(repulsionStrengthFlag, repulsionStrengthLongFlag, MSyntax::kDouble);
+	syntax.addFlag(repulsionSteepFlag, repulsionSteepLongFlag, MSyntax::kDouble);
+	syntax.addFlag(expandFlag, expandLongFlag, MSyntax::kLong);
+	syntax.addFlag(keepFlag, keepLongFlag, MSyntax::kLong);
+	syntax.addFlag(lambdaFlag, lambdaLongFlag, MSyntax::kDouble);
+	syntax.addFlag(thicknessFlag, thicknessLongFlag, MSyntax::kDouble);
+
 	return syntax;
 }
 
 // Plugin doIt function
 MStatus WireSculpt::doIt(const MArgList& argList)
 {
-	MGlobal::displayInfo("about to add script");
-
 	MStatus status;
 
-	MString firstFlagArg;
-	unsigned int secondFlagArg{};
+	MString file = "";
+	double attractStrength = 1.0;
+	double attractSteep = 0.0;
+	double repulStrength = 1.0;
+	double repulSteep = 0.0;
+	int expand = 0; 
+	int keep = 0; 
+	double lambda = 1.0; 
+	double thickness = 1.0;
+
 
 	MArgParser argData(syntax(), argList, &status);
 
-	if (argData.isFlagSet(nameFlag)) {
-		argData.getFlagArgument(nameFlag, 0, firstFlagArg);
+	if (argData.isFlagSet(fileFlag)) {
+		argData.getFlagArgument(fileFlag, 0, file);
 	}
 	else {
-		MGlobal::displayInfo("Flag -name is NOT set.");
+		MGlobal::displayInfo("Flag -fp is NOT set.");
 	}
 
-	if (argData.isFlagSet(idFlag)) {
-		argData.getFlagArgument(idFlag, 0, secondFlagArg);
+	if (argData.isFlagSet(attractStrengthFlag)) {
+		argData.getFlagArgument(attractStrengthFlag, 0, attractStrength);
 	}
 	else {
-		MGlobal::displayInfo("Flag -id is NOT set.");
+		MGlobal::displayInfo("Flag -asr is NOT set.");
 	}
 
-	MGlobal::displayInfo("pop-up menu");
+	if (argData.isFlagSet(attractSteepFlag)) {
+		argData.getFlagArgument(attractSteepFlag, 0, attractSteep);
+	}
+	else {
+		MGlobal::displayInfo("Flag -ase is NOT set.");
+	}
 
-	MString result;
+	if (argData.isFlagSet(repulsionStrengthFlag)) {
+		argData.getFlagArgument(repulsionStrengthFlag, 0, repulStrength);
+	}
+	else {
+		MGlobal::displayInfo("Flag -rsr is NOT set.");
+	}
 
-	MString dialog = "confirmDialog -title \"Hello Maya\" -message \"Name: ";
-	dialog += firstFlagArg;
-	dialog += "\\nID: ";
-	dialog += secondFlagArg;
-	dialog += "\" -button \"OK\" -defaultButton \"OK\" -dismissString \"Cancel\";";
-	MGlobal::executeCommand(dialog, result);
+	if (argData.isFlagSet(repulsionSteepFlag)) {
+		argData.getFlagArgument(repulsionSteepFlag, 0, repulSteep);
+	}
+	else {
+		MGlobal::displayInfo("Flag -rse is NOT set.");
+	}
+
+	if (argData.isFlagSet(expandFlag)) {
+		argData.getFlagArgument(expandFlag, 0, expand);
+	}
+	else {
+		MGlobal::displayInfo("Flag -e is NOT set.");
+	}
+
+	if (argData.isFlagSet(keepFlag)) {
+		argData.getFlagArgument(keepFlag, 0, keep);
+	}
+	else {
+		MGlobal::displayInfo("Flag -k is NOT set.");
+	}
+
+	if (argData.isFlagSet(lambdaFlag)) {
+		argData.getFlagArgument(lambdaFlag, 0, lambda);
+	}
+	else {
+		MGlobal::displayInfo("Flag -l is NOT set.");
+	}
+
+	if (argData.isFlagSet(thicknessFlag)) {
+		argData.getFlagArgument(thicknessFlag, 0, thickness);
+	}
+	else {
+		MGlobal::displayInfo("Flag -t is NOT set.");
+	}
+
+	// flag values 
+	MString print = "filepath: ";
+	print += file;
+	print += "\n attract stren: ";
+	print += attractStrength;
+	print += "\n attract steep: ";
+	print += attractSteep;
+	print += "\n repulsion stren: ";
+	print += repulStrength;
+	print += "\n repulsion steep: ";
+	print += repulSteep;
+	print += "\n expand: ";
+	print += expand;
+	print += "\n keep: ";
+	print += keep;
+	print += "\n lambda: ";
+	print += lambda;
+	print += "\n thickness: ";
+	print += thickness;
+	MGlobal::displayInfo(print);
 
 	return MStatus::kSuccess;
 }
