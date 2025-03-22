3c2681d2e16cd60c990f2a2ee7d9658c83ccbce2
Neha Thumu <71038542+thumun@users.noreply.github.com>
2025-03-21 21:09:29 -0400

    extreme pts logic ported over

---
+++ WireSculpt/ExtremePoints.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <vector>
+#include <Eigen/Core>
+
+void compute_all_features(Eigen::MatrixXd& V, Eigen::MatrixXi& F, Eigen::MatrixXi& E, Eigen::MatrixXd& N,
+	std::vector<std::vector<int>>& VF,
+	std::vector<std::vector<int>>& VFi, Eigen::MatrixXi& IF, Eigen::MatrixXi& OV,
+	Eigen::MatrixXd& FC,
+	Eigen::MatrixXd& FN, Eigen::MatrixXd& DA, Eigen::MatrixXd& D, Eigen::MatrixXd& L,
+	Eigen::MatrixXd& G, Eigen::MatrixXd& dblA);
+void compute_laplacian(Eigen::MatrixXd& V, Eigen::MatrixXi& F, Eigen::MatrixXi& E, Eigen::MatrixXd& G,
+	Eigen::MatrixXd& N, Eigen::MatrixXd& L, Eigen::MatrixXd& vertex_is_concave, double beta,
+	double eps, double sigma, int clip_bound, int lap_weighting, double filter1_thresh);
+std::vector<int> get_extreme_points(Eigen::MatrixXi& F, Eigen::MatrixXd& V, Eigen::MatrixXd& L, int index_given,
+	Eigen::MatrixXi& E);
--- WireSculpt/WireSculptPlugin.h
+++ WireSculpt/WireSculptPlugin.h
@@ -21,16 +21,6 @@ private:
 
 	std::vector<std::string> SplitString(const std::string& input, char delimiter);
 	bool GetFileExtension(const std::string& filePath);
-
-	void compute_all_features(Eigen::MatrixXd& V, Eigen::MatrixXi& F, Eigen::MatrixXi& E, Eigen::MatrixXd& N,
-		std::vector<std::vector<int>>& VF,
-		std::vector<std::vector<int>>& VFi, Eigen::MatrixXi& IF, Eigen::MatrixXi& OV,
-		Eigen::MatrixXd& FC,
-		Eigen::MatrixXd& FN, Eigen::MatrixXd& DA, Eigen::MatrixXd& D, Eigen::MatrixXd& L,
-		Eigen::MatrixXd& G, Eigen::MatrixXd& dblA);
-	void compute_laplacian(Eigen::MatrixXd& V, Eigen::MatrixXi& F, Eigen::MatrixXi& E, Eigen::MatrixXd& G,
-		Eigen::MatrixXd& N, Eigen::MatrixXd& L, Eigen::MatrixXd& vertex_is_concave, double beta,
-		double eps, double sigma, int clip_bound, int lap_weighting, double filter1_thresh);
 };
 
 
