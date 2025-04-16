c90d23d111d3bd58f3a21fabdb783068b750ad40
ClaireL21 <53claire@gmail.com>
2025-04-15 21:13:31 -0400

    clamped colors

--- WireSculpt/WireSculptNode.cpp
+++ WireSculpt/WireSculptNode.cpp
@@ -415,9 +415,11 @@ void WireSculptNode::createHeatMapMesh(const double& radius, std::unordered_map<
 
         for (unsigned int i = 0; i < numVerticesThisSphere; ++i) {
             float distance = colorScheme[(vc.first)];
-
-            r = 1.0 * distance / maxDist;
-            b = 1.0 - 1.0 * distance / maxDist;
+            r = distance; //(maxDist > 1e-6f) ? (distance / maxDist) : 0.0f;
+            r = std::min(std::max(r, 0.0f), 1.0f);
+            b = 1.0 - r;
+           /* r = 1.0 * distance / maxDist;
+            b = 1.0 - 1.0 * distance / maxDist;*/
 
             MColor color(r, g, b);
             (*colors).append(color);
